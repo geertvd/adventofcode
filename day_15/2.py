808f13096028e022b56a8f0b4f69a05b326a6deb
Geert van Dort <geert@xio.be>
2015-12-15 12:38:09 +0100

    day 15

---
+++ day_15/2.py
@@ -0,0 +1,46 @@
+import re, itertools
+input = file('input.txt')
+
+scoreregex = re.compile(r'(.*): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)')
+
+ingredients = {}
+for line in input:
+    ingredient, cap, dur, flav, texture, cal = scoreregex.findall(line)[0]
+    ingredients[ingredient] = {
+        'cap': int(cap),
+        'dur': int(dur),
+        'flav': int(flav),
+        'texture': int(texture),
+        'cal': int(cal)
+    }
+
+def sum_to_100(numbers, count):
+    combos = []
+    for combo in itertools.combinations(numbers, count):
+        if sum(combo) == 100:
+            combos.append(combo)
+    return combos
+
+def calculate_points(ingredients):
+    cap = dur = flav = texture = cal = 0
+    for ingredient_name in ingredients:
+        ingredient = ingredients[ingredient_name]
+        cap += ingredient['cap'] * ingredient['tsp']
+        dur += ingredient['dur'] * ingredient['tsp']
+        flav += ingredient['flav'] * ingredient['tsp']
+        texture += ingredient['texture'] * ingredient['tsp']
+        cal += ingredient['cal'] * ingredient['tsp']
+    if cal == 500:
+        return max(cap, 0) * max(dur, 0) * max(flav, 0) * max(texture, 0)
+    return 0
+
+ratios = sum_to_100(range(1,100), len(ingredients))
+maxpoints = 0
+for permutation in itertools.permutations(ingredients):
+    for ratio in ratios:
+        recipe = {}
+        for i, ingredient in enumerate(permutation):
+            recipe[ingredient] = ingredients[ingredient]
+            recipe[ingredient]['tsp'] = ratio[i]
+        maxpoints = max(calculate_points(recipe), maxpoints)
+print maxpoints
